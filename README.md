54e745764d4d5747c3e60f07ae5dcbdaa5198cf9
Adrian Estrada <edsadr@gmail.com>
2015-03-27 16:15:36 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Example and really quick to build REST API
+
+Just an example app, a REST API using mongoose, get it working in 5 minutes
---
+++ index.js
@@ -0,0 +1,45 @@
+var restify = require('restify');
+var restifyMongoose = require('restify-mongoose');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/directoryApi');
+
+var server = restify.createServer({
+  name: 'restify directory REST API',
+  version: '1.0.0'
+});
+
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+server.use(restify.CORS());
+
+
+var DirectorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },  
+  phone: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  date: {
+    type: Date,    
+    default: Date.now
+  }
+});
+
+var Directory = mongoose.model('directory', DirectorySchema);
+
+restifyMongoose(Directory).serve('/api/directory', server);
+
+server.listen(4500, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
